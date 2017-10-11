3a06014f84e568f0360b37d4a12e1a916be325bd
Diogo Cró <diogocror@gmail.com>
2017-10-11 01:22:23 +0100

    first commit

---
+++ ConcertGenerator/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ConcertGenerator
+{
+    [Activity(Label = "ConcertGenerator", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button _btnInsert;
+        ObservableCollection<string> someList = new ObservableCollection<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            someList.Add("teste");
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            _btnInsert = (Button)FindViewById(Resource.Id.BtnInsert);
+
+            _btnInsert.Click += InsertAction;
+
+        }
+
+        private void InsertAction(object sender, EventArgs e)
+        {
+            TextView txtSongName = (TextView)FindViewById(Resource.Id.TxtMusicName);
+            LinearLayout svSongsList = (LinearLayout)FindViewById(Resource.Id.LlSongsList);
+
+            if (txtSongName.Text.Length == 0)
+                return;
+            TextView v = new TextView(ApplicationContext) { Text = txtSongName.Text };
+            v.LongClick += (s, es) => { txtSongName.Text = v.Text; };
+            v.Clickable = true;
+
+            svSongsList.AddView(v);
+
+            txtSongName.Text = "";
+        }
+    }
+}
+
