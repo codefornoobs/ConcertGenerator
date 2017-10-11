8b4df136e90b667971a63cc23bee2dd144fc5ebb
Diogo Cró <diogocror@gmail.com>
2017-10-11 17:01:46 +0100

    deleting songs

--- ConcertGenerator/MainActivity.cs
+++ ConcertGenerator/MainActivity.cs
@@ -2,7 +2,7 @@
 using Android.OS;
 using Android.Widget;
 using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace ConcertGenerator
 {
@@ -10,17 +10,18 @@ namespace ConcertGenerator
     public class MainActivity : Activity
     {
         private Button _btnInsert;
-        ObservableCollection<string> someList = new ObservableCollection<string>();
 
+        private IList<string> Songs;
         protected override void OnCreate(Bundle savedInstanceState)
         {
 
             base.OnCreate(savedInstanceState);
-            someList.Add("teste");
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            _btnInsert = (Button)FindViewById(Resource.Id.BtnInsert);
 
+            Songs = new List<string>();
+
+            _btnInsert = (Button)FindViewById(Resource.Id.BtnInsert);
             _btnInsert.Click += InsertAction;
 
         }
@@ -32,14 +33,24 @@ namespace ConcertGenerator
 
             if (txtSongName.Text.Length == 0)
                 return;
+
             TextView v = new TextView(ApplicationContext) { Text = txtSongName.Text };
-            v.LongClick += (s, es) => { txtSongName.Text = v.Text; };
+            v.LongClick += (s, es) =>
+            {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Delete Song");
+                alert.SetMessage("Do you want to delete this song?\n" + $"  \"{v.Text}\"");
+                alert.SetPositiveButton("Delete", (d, w) => { svSongsList.RemoveView(v); alert.Dispose(); });
+                alert.SetNegativeButton("Cancel", (d, w) => { alert.Dispose(); });
+                alert.Show();
+            };
             v.Clickable = true;
 
             svSongsList.AddView(v);
-
+            Songs.Add(v.Text);
             txtSongName.Text = "";
         }
+
     }
 }
 
