7102e68461ef23c38da9b800fc383ab0f58c873c
Code for Boobs <hello@codefornoobs.com>
2017-10-12 00:38:10 +0100

    Player and Song objects created

---
+++ ConcertGenerator/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcertGenerator
+{
+    class Player
+    {
+        public string Name;
+        public enum InstrumentType { Voice, Guitar, GuitarPT };
+
+        public int Instrument;
+        public Player(string name, InstrumentType instrument)
+        {
+            Name = name;
+            Instrument = (int)instrument;
+        }
+    }
+}
---
+++ ConcertGenerator/Song.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ConcertGenerator
+{
+    class Song
+    {
+        public string Name;
+        public bool MustPlayInConcert;
+        public List<Player> Players;
+    }
+}
