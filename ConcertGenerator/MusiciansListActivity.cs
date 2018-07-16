9ee699d89f7fd3ae0bac682ca93ad4cd8617fbd1
Diogo Cró <diogocror@gmail.com>
2018-07-16 09:10:09 +0100

    List all musicians activity

---
+++ ConcertGenerator/MusiciansListActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace ConcertGenerator
+{
+    [Activity(Label = "MusiciansListActivity")]
+    public class MusiciansListActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+          
+
+            // Create your application here
+            SetContentView(Resource.Layout.MusiciansList);
+
+            var test = Intent.GetSerializableExtra("players");
+            var player = JsonConvert.DeserializeObject<List<Player>>(test.ToString());
+            var dd = 0;
+        }
+    }
+}
