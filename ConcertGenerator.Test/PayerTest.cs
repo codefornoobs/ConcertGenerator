992a069e6537854e70063664e27cb7073ca28423
Diogo Cró <diogocror@gmail.com>
2018-07-16 09:08:17 +0100

    tests project

---
+++ ConcertGenerator.Test/PayerTest.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertGenerator.Test
+{
+    [TestFixture]
+    public class PayerTest
+    {
+        [Test]
+        public void NewPlayerTest()
+        {
+            var newPlayer = new Player("Diogo");
+
+            Assert.AreEqual("Diogo", newPlayer.Name);
+            Assert.AreEqual(0, newPlayer.GetAllInstruments().Count);
+        }
+
+
+        [Test]
+        public void AddNewGuitarInstrument()
+        {
+            var newPlayer = new Player("Diogo");
+
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+          
+            Assert.AreEqual(1, newPlayer.GetAllInstruments().Count);
+            Assert.AreEqual(InstrumentType.Guitar, newPlayer.GetAllInstruments()[0]);
+        }
+        [Test]
+        public void AddNewGuitarPtAndVoiceInstruments()
+        {
+            var newPlayer = new Player("Diogo");
+
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+            newPlayer.AddNewIntrument(InstrumentType.Voice);
+
+            Assert.AreEqual(2, newPlayer.GetAllInstruments().Count);
+            Assert.AreEqual(InstrumentType.Guitar, newPlayer.GetAllInstruments()[0]);
+            Assert.AreEqual(InstrumentType.Voice, newPlayer.GetAllInstruments()[1]);
+        }
+
+        [Test]
+        public void AddNewInstrumentsMultipletimes()
+        {
+            var newPlayer = new Player("Diogo");
+
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+            newPlayer.AddNewIntrument(InstrumentType.GuitarPT);
+
+            Assert.AreEqual(2, newPlayer.GetAllInstruments().Count);
+            Assert.AreEqual(InstrumentType.Guitar, newPlayer.GetAllInstruments()[0]);
+            Assert.AreEqual(InstrumentType.GuitarPT, newPlayer.GetAllInstruments()[1]);
+        }
+        [Test]
+        public void RemoveInstrumentGuitar()
+        {
+            var newPlayer = new Player("Diogo");
+
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+            newPlayer.AddNewIntrument(InstrumentType.GuitarPT);
+            newPlayer.RemoveInstrument(InstrumentType.Guitar);
+
+            Assert.AreEqual(1, newPlayer.GetAllInstruments().Count);
+            Assert.AreEqual(InstrumentType.GuitarPT, newPlayer.GetAllInstruments()[0]);
+        }
+        [Test]
+
+        public void RemoveAllInstruments()
+        {
+            var newPlayer = new Player("Diogo");
+
+            newPlayer.AddNewIntrument(InstrumentType.Guitar);
+            newPlayer.AddNewIntrument(InstrumentType.Voice);
+            newPlayer.AddNewIntrument(InstrumentType.GuitarPT);
+
+            Assert.AreEqual(3, newPlayer.GetAllInstruments().Count);
+
+            newPlayer.RemoveAllInstruments();
+
+            Assert.AreEqual(0, newPlayer.GetAllInstruments().Count);
+
+        }
+    }
+}
