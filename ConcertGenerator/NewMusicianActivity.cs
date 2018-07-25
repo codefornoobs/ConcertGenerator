909b7365eaca526e2ce64ee297a2b9c8cf9917bb
Diogo Cró <diogocror@gmail.com>
2018-07-25 09:03:13 +0100

    Add new players with controller

--- ConcertGenerator/NewMusicianActivity.cs
+++ ConcertGenerator/NewMusicianActivity.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using ConcertGenerator.Controllers;
+using ConcertGenerator.Models;
 using Newtonsoft.Json;
 
 namespace ConcertGenerator
@@ -16,7 +18,6 @@ namespace ConcertGenerator
     [Activity(Label = "Add Musician", MainLauncher = true)]
     public class NewMusicianActivity : Activity
     {
-        private List<Player> _players = new List<Player>();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,10 +27,12 @@ namespace ConcertGenerator
             SetContentView(Resource.Layout.NewMusician);
 
             var musiciansListButton = (Button)FindViewById(Resource.Id.btnListMusician);
-            var AddNewMusician = (Button)FindViewById(Resource.Id.btnAddNewMusician);
+            var addNewMusician = (Button)FindViewById(Resource.Id.btnAddNewMusician);
 
             musiciansListButton.Click += LoadMusiciansListActivity;
-            AddNewMusician.Click += AddNewMusicianOnClick;
+            addNewMusician.Click += AddNewMusicianOnClick;
+
+            PlayerApiController.InitializeDatabase();
         }
 
         private void AddNewMusicianOnClick(object sender, EventArgs e)
@@ -39,22 +42,33 @@ namespace ConcertGenerator
             var cbGuitar= (CheckBox)FindViewById(Resource.Id.cbGuitar);
             var cbGuitarPt = (CheckBox)FindViewById(Resource.Id.cbBass);
 
-           var newPlayer = new Player(playerName.Text);
+            var playerApi = new PlayerApiController();
 
+
+            var instruments = CheckInstrumentsSelected(cbVoice, cbGuitar, cbGuitarPt);
+
+            playerApi.AddNewPlayer(playerName.Text,instruments);
+        }
+
+        private static List<InstrumentType> CheckInstrumentsSelected(CheckBox cbVoice, CheckBox cbGuitar, CheckBox cbGuitarPt)
+        {
+            var instruments = new List<InstrumentType>();
             if (cbVoice.Checked)
-                newPlayer.AddNewIntrument(InstrumentType.Voice);
+                instruments.Add(InstrumentType.Voice);
             if (cbGuitar.Checked)
-                newPlayer.AddNewIntrument(InstrumentType.Guitar);
+                instruments.Add(InstrumentType.Guitar);
             if (cbGuitarPt.Checked)
-                newPlayer.AddNewIntrument(InstrumentType.GuitarPT);
+                instruments.Add(InstrumentType.GuitarPt);
 
-            _players.Add(newPlayer);
+            return instruments;
         }
 
         private void LoadMusiciansListActivity(object sender, EventArgs e)
         {
+            var playerApi = new PlayerApiController();
+
             var intent = new Intent(this,typeof(MusiciansListActivity));
-            intent.PutExtra("players", JsonConvert.SerializeObject(_players));
+            intent.PutExtra("players", JsonConvert.SerializeObject(playerApi.GetAllPlayers()));
 
             StartActivity(intent);
         }
