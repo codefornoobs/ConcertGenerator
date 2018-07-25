21f8760756ddaa240891fa4d36115e17eb3f8dbe
Diogo Cró <diogocror@gmail.com>
2018-07-25 09:01:11 +0100

    Moved models to Models folder

---
+++ ConcertGenerator/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SQLite;
+
+namespace ConcertGenerator.Models
+{
+    public enum InstrumentType { Voice, Guitar, GuitarPt };
+
+   public class Player
+    {
+        [PrimaryKey]
+        public string Name { get; set; }
+
+        public List<InstrumentType> Instruments { get; set; }
+       
+      
+    }
+}
--- ConcertGenerator/Models/Song.cs
+++ ConcertGenerator/Models/Song.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using ConcertGenerator.Models;
 
 namespace ConcertGenerator
 {
