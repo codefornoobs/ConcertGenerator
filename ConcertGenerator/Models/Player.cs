8986f42d6c70be6bcc54626f4dc352002e8dfe7d
Diogo Cró <diogocror@gmail.com>
2018-07-30 22:36:27 +0100

    removed intrumets and put in new class

--- ConcertGenerator/Models/Player.cs
+++ ConcertGenerator/Models/Player.cs
@@ -3,15 +3,13 @@ using SQLite;
 
 namespace ConcertGenerator.Models
 {
-    public enum InstrumentType { Voice, Guitar, GuitarPt };
+
 
    public class Player
     {
         [PrimaryKey]
         public string Name { get; set; }
 
-        public List<InstrumentType> Instruments { get; set; }
-       
-      
+ 
     }
 }
